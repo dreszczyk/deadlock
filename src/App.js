fcd3b47957d85fe338df71d052c9b3e87a65cf67
dreszczyk <marcin@plamka.net>
2019-08-24 15:15:54 +0200

    testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+
 
 function App() {
-  return (
-    <div>
-      hej :)
-    </div>
-  );
+    return (
+        <div>
+            <Button type="primary">hej :)</Button>
+        </div>
+    );
 }
 
 export default App;
